63b3e11bf109a75d9496abe52371e94e27e84e4b
Joao Soeiro <joao.soeiro@sumup.com>
2024-02-26 18:17:18 -0300

    Commiting finished GO/ REST API project made for study purposes.

---
+++ backend/migration/docker-database-initial.sql
@@ -0,0 +1,9 @@
+create table personalidades(
+    id serial primary key,
+    nome varchar,
+    historia varchar
+);
+
+INSERT INTO personalidades(nome, historia) VALUES
+('Arthas Menethil', 'Son of Therenas Menethil, prince of Lordaeron, eventually became the most badass Lich King Ever.'),
+('Jaina Proudmoore', 'Badass mage, went crazy and got killed during BFA, lol.');
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jsoeirosmp/go_rest_api_basic_project/backend/database"
+	"github.com/jsoeirosmp/go_rest_api_basic_project/backend/models"
+	"github.com/jsoeirosmp/go_rest_api_basic_project/backend/routes"
+)
+
+func main() {
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
+	}
+	database.ConectaComBancoDeDados()
+	fmt.Println("Iniciando o servidor Rest com Go")
+	routes.HandleResquest()
+}
